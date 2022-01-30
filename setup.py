a2a577d64a8feb08b9e9c0ed66534ef444953f74
Max Pershin <mepershin@gmail.com>
2022-01-30 18:46:13 +0300

    update setup py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
 description = """Pytorch version of resnet50 and resnet200 checkpoints of
 "Efficient Visual Pretraining with Contrastive Detection" model"""
 
 
-setuptools.setup(
+setup(
     name="pytorch_eff_vis_pretraining/download.py",
     version="1.0.0",
     author="Pershin Maxim",
@@ -21,10 +21,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "pytorch_eff_vis_pretraining"},
-    packages=[
-            'torch',
-            'tqdm',
-        ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     python_requires=">=3.8",
 )
